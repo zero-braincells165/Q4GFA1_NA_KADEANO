91c468279b9e20f47a992e71be9061156bc6fade
YuriLivia <143151116+zero-braincells165@users.noreply.github.com>
2024-04-08 16:31:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function displayUserData(userData) {
   let outputHtml = '<h2>Submitted Information:</h2>';
   outputHtml += '<p>';
   for (const key in userData) {
-    outputHtml += `${key}: ${userData[key]}<br>`;
+    outputHtml += `<strong>${key}:</strong> ${userData[key]}<br>`;
   }
   outputHtml += '</p>';
   outputDiv.innerHTML = outputHtml;
